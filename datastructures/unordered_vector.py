e5aaa7a1ce92ab67c0b9a2234dc4e04a1b8c91d5
Fredson Chaves <fredsonchaves07@gmail.com>
2021-07-08 14:34:22 -0300

    refact: making variables private

--- datastructures/unordered_vector.py
+++ datastructures/unordered_vector.py
@@ -1,17 +1,17 @@
 class UnorderedVector:
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.last_position = -1
-        self.values = []
+        self.__capacity = capacity
+        self.__last_position = -1
+        self.__values = []
 
     def is_empty(self):
-        if self.last_position == -1:
+        if self.__last_position == -1:
             return True
 
         return False
 
     def is_capacity_max(self):
-        if self.last_position == self.capacity - 1:
+        if self.__last_position == self.__capacity - 1:
             return True
 
         return False
@@ -20,25 +20,25 @@ class UnorderedVector:
         if self.is_capacity_max():
             print("Capacidade máxima atiginda")
         else:
-            self.values.insert(self.last_position + 1, value)
-            self.last_position += 1
+            self.__values.insert(self.__last_position + 1, value)
+            self.__last_position += 1
 
     def search(self, value):
         if self.is_empty():
             print("O Vetor está vazio")
 
-        for i in range(self.last_position + 1):
-            if value == self.values[i]:
+        for i in range(self.__last_position + 1):
+            if value == self.__values[i]:
                 print(f"{i} - {value}")
 
     def remove(self, value):
         aux_values = []
 
-        for i in range(self.last_position + 1):
-            if self.values[i] != value:
-                aux_values.append(self.values[i])
+        for i in range(self.__last_position + 1):
+            if self.__values[i] != value:
+                aux_values.append(self.__values[i])
 
         for i in range(len(aux_values)):
-            self.values[i] = aux_values[i]
+            self.__values[i] = aux_values[i]
 
-        self.last_position = len(aux_values) - 1
+        self.__last_position = len(aux_values) - 1
